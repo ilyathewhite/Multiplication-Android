2ee5bfa8e0bb8ca6a6d5001146982fb6b5793f9f
sergey <sergey.shlepakov@gmail.com>
2018-11-02 17:03:17 +0300

    Test animation

--- app/src/main/java/ru/mathtasks/multiplicationtable/CellView.kt
+++ app/src/main/java/ru/mathtasks/multiplicationtable/CellView.kt
@@ -1,48 +1,42 @@
 package ru.mathtasks.multiplicationtable
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.drawable.ClipDrawable
 import android.graphics.drawable.Drawable
-import android.text.TextPaint
-import android.util.AttributeSet
-import android.view.View
+import android.graphics.drawable.LayerDrawable
+import android.view.Gravity
 import android.widget.ImageView
 
-enum class CellState
-{
-    Empty, Filled, ToBeFilled, WasEmptied
-}
+enum class CellState { Empty, Filled, ToBeFilled, WasEmptied }
+enum class CellAnimationDirection { LeftToRight, TopToBottom }
+
 class CellView : ImageView {
-    var State: CellState = CellState.Empty;
+    private var drawable2: ClipDrawable? = null
+    private var drawable1: ClipDrawable? = null
 
-    constructor(context: Context) : super(context) {
-        init(null, 0)
+    constructor(context: Context, state: CellState) : super(context) {
+        setImageDrawable(cell(state))
     }
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        init(attrs, 0)
+    constructor(context: Context, state1: CellState, state2: CellState, dir: CellAnimationDirection) : super(context) {
+        val orientation = if(dir == CellAnimationDirection.LeftToRight) ClipDrawable.HORIZONTAL else ClipDrawable.VERTICAL
+        drawable1 = ClipDrawable(cell(state1), if(dir == CellAnimationDirection.LeftToRight) Gravity.RIGHT else Gravity.BOTTOM, orientation) . apply { level = 10000 }
+        drawable2 = ClipDrawable(cell(state2), if(dir == CellAnimationDirection.LeftToRight) Gravity.LEFT else Gravity.TOP, orientation) .apply { level = 0 }
+        setImageDrawable(LayerDrawable(arrayOf(drawable1, drawable2)))
     }
 
-    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
-        init(attrs, defStyle)
-    }
+    var level = 0
+        set(value) {
+            if (drawable1 != null)
+                drawable1!!.level = 10000 - value
+            if (drawable2 != null)
+                drawable2!!.level = value
+        }
 
-    private fun init(attrs: AttributeSet?, defStyle: Int) {
-        background = getResources().getDrawable(R.drawable.cell_view)
-    }
-
-    override fun onCreateDrawableState(extraSpace: Int): IntArray {
-        val drawableState = super.onCreateDrawableState(extraSpace + 1)
-        if (State == CellState.Empty)
-            View.mergeDrawableStates(drawableState, intArrayOf(R.attr.state_empty))
-        if (State == CellState.Filled)
-            View.mergeDrawableStates(drawableState, intArrayOf(R.attr.state_filled))
-        if (State == CellState.ToBeFilled)
-            View.mergeDrawableStates(drawableState, intArrayOf(R.attr.state_to_be_filled))
-        if (State == CellState.WasEmptied)
-            View.mergeDrawableStates(drawableState, intArrayOf(R.attr.state_was_emptied))
-        return drawableState
-    }
+    private fun cell(s: CellState) = resources.getDrawable(when (s) {
+        CellState.Empty -> R.drawable.cell_empty
+        CellState.Filled -> R.drawable.cell_filled
+        CellState.ToBeFilled -> R.drawable.cell_to_be_filled
+        CellState.WasEmptied -> R.drawable.cell_was_emptied
+    })
 }
--- app/src/main/java/ru/mathtasks/multiplicationtable/TrainingActivity.kt
+++ app/src/main/java/ru/mathtasks/multiplicationtable/TrainingActivity.kt
@@ -1,28 +1,29 @@
 package ru.mathtasks.multiplicationtable
 
+import android.animation.ValueAnimator
 import android.app.Activity
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-
-import kotlinx.android.synthetic.main.activity_training.*
+import java.lang.Math.min
 
 const val TRAINING_ACTIVITY_MULTIPLICAND = "ru.mathtasks.multiplicationtable.trainingactivity.multiplicand"
 
 class TrainingActivity : Activity() {
 
-    private var multiplier = 0;
-    private var multiplicand = 0;
-    private var answer : Int? = null;
+    private var multiplier = 0
+    private var multiplicand = 0
+    private var answer : Int? = null
+    private val LEFT_INDENT = 30
+    private val RIGHT_INDENT = 40
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_training)
 
-        this.multiplier = 1
+        val prevMultiplier = 3
+        this.multiplier = 5
         this.multiplicand = intent.getIntExtra(TRAINING_ACTIVITY_MULTIPLICAND, 0)
 
         class B(val button_id:Int, val value: Int);
@@ -40,23 +41,52 @@ class TrainingActivity : Activity() {
         for(b in buttons)
             findViewById<Button>(b.button_id).setOnClickListener { _ -> onNumKey(b.value)}
 
-        findViewById<Button>(R.id.btn_bs).setOnClickListener { _ -> onBsKey() }
+        findViewById<Button>(R.id.btn_bs).setOnClickListener { _ -> onBsKey()}
         updateTask()
 
         val field = findViewById<LinearLayout>(R.id.ll_field)
+        field.post {
+            val dim = min(field.width - LEFT_INDENT - RIGHT_INDENT, field.height) / 10
 
-        for(m in 1..10) {
-            val hl = LinearLayout(this) .apply {
-                layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-                orientation = android.widget.LinearLayout.HORIZONTAL
-            }
-            field.addView(hl)
-            for(n in 1..multiplicand) {
-                val v1 = CellView(this) .apply {
-                    State = when(n % 4) { 0 -> CellState.Empty 1 -> CellState.Filled 2 -> CellState.WasEmptied else -> CellState.ToBeFilled }
-                    layoutParams = ViewGroup.LayoutParams(60, 60)
+            var animatedCells = mutableListOf<CellView>()
+            for(m in 1..10) {
+                val hl = LinearLayout(this).apply {
+                    layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                    orientation = android.widget.LinearLayout.HORIZONTAL
                 }
-                hl.addView(v1)
+                field.addView(hl)
+                val number = TextView(this).apply {
+                    text = m.toString()
+                    layoutParams = ViewGroup.LayoutParams(LEFT_INDENT, dim)
+                    setTextColor(resources.getColor(if (m <= multiplier) R.color.fieldNumberActive else R.color.fieldNumberInactive))
+                    textAlignment = View.TEXT_ALIGNMENT_VIEW_END
+                    setPadding(0, 0, 10, 0)
+                }
+                hl.addView(number)
+
+                for(n in 1..multiplicand) {
+                    val v1 = (when {
+                        m <= prevMultiplier -> CellView(this, CellState.Filled)
+                        m <= multiplier -> CellView(this, CellState.ToBeFilled, CellState.Filled, CellAnimationDirection.TopToBottom)
+                        else -> CellView(this, CellState.Empty)
+                    }).apply {
+                        setPadding(3,3,3,3)
+                        layoutParams = ViewGroup.LayoutParams(dim, dim)
+                    }
+                    if(m in prevMultiplier..multiplier)
+                        animatedCells.add(v1)
+                    hl.addView(v1)
+                }
+            }
+
+            val va = ValueAnimator.ofInt(0, 10000).apply {
+                addUpdateListener {
+                    val value = animatedValue as Int
+                    for(c in animatedCells)
+                        c.level = value
+                }
+                duration = 1000
+                start()
             }
         }
     }
